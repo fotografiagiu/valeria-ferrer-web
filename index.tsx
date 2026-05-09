af212b553a621205732528e8ebe616f907ea2e58
fotografiagiu <fotografiapgiu@gmail.com>
2026-05-09 12:47:30 +0200

    Eliminar script de limpieza temporal de Service Worker
    - El script estaba afectando negativamente la experiencia
    - La web se mantenía igual para usuarios antiguos
    - Mejor dejar que el navegador maneje la cache normalmente
    - Service Worker ya está desactivado (sw.js.disabled)
    - Vercel manejará cache de assets estáticos con headers

--- index.tsx
+++ index.tsx
@@ -4,26 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// LIMPIEZA TEMPORAL DE SERVICE WORKER Y CACHES ANTIGUOS
-// Eliminar después de unos días cuando todos los usuarios hayan actualizado
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((registration) => {
-      console.log('Unregistering old service worker:', registration.scope);
-      registration.unregister();
-    });
-  });
-}
-
-if ('caches' in window) {
-  caches.keys().then((cacheNames) => {
-    cacheNames.forEach((cacheName) => {
-      console.log('Deleting old cache:', cacheName);
-      caches.delete(cacheName);
-    });
-  });
-}
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
